1af90bf01a59a4c264384cade99218733a9493f1
meija1 <meija911@gmail.com>
2023-05-25 16:41:47 +0000

    Fixing heroku app

--- byteblog/wsgi.py
+++ byteblog/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'byte_blog.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'byteblog.settings')
 
 application = get_wsgi_application()
