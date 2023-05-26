0919ec2b250a65c0e5af55be3d8ad4afc82491d9
meija1 <meija911@gmail.com>
2023-05-26 15:35:33 +0000

    Create comment model

--- blog/models.py
+++ blog/models.py
@@ -16,5 +16,8 @@ class Post(models.Model):
     status = models.IntegerField(choices=STATUS, default=0)
     likes = models.ManyToManyField(User, related_name='blog_likes', blank=True)
 
-    
+class Meta:
+    ordering = ['-created_on']
 
+def __str__(self):
+    return f"Comment {self.body} by {self.name}"
