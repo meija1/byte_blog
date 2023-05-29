0ba117ee6cae1d260cc55876d4c57ffe46c79ea3
meija1 <meija911@gmail.com>
2023-05-29 14:40:30 +0000

    Add comment functions for admin and create Comment class in models

--- blog/models.py
+++ blog/models.py
@@ -16,8 +16,27 @@ class Post(models.Model):
     status = models.IntegerField(choices=STATUS, default=0)
     likes = models.ManyToManyField(User, related_name='blog_likes', blank=True)
 
-class Meta:
-    ordering = ['-created_on']
 
-def __str__(self):
-    return f"Comment {self.body} by {self.name}"
+    class Meta:
+        ordering = ["-created_on"]
+
+    def __str__(self):
+        return self.title
+
+    def number_of_likes(self):
+        return self.likes.count()
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    name = models.EmailField()
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.name}"
