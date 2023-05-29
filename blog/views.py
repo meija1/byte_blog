bfee4de7c654b5e31455e6663cbe4eb73f2f87b4
meija1 <meija911@gmail.com>
2023-05-29 15:06:04 +0000

    Add the view code in views.py for website

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Post
 
-# Create your views here.
+
+''' Create a basic view for the website '''
+class PostList(generic.ListView):
+    model = Post
+    queryset = Post.objects.filter(status=1).order_by('-created_on')
+    template_name = 'index.html'
+    paginate_by = 6
