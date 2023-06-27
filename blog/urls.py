980fda4199ddcb305e47765f219b803d1bbb5eb5
meija1 <meija911@gmail.com>
2023-06-27 16:35:40 +0000

    Add function to submit and approve comments

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,10 @@
 from . import views
 from django.urls import path
+from .views import PostUpload
 
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
+    path('<slug:slug>/', views.PostUpload.as_view(), name='create_post'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -24,18 +24,41 @@ class PostDetail(View):
                 liked =True
 
             return render(
-                request, "post_detail.html", {"post": post, "comments": comments, "liked": liked, "comment_form": CommentForm()},
+                request, "post_detail.html", {"post": post, "comments": comments, "commented": False, "liked": liked, "comment_form": CommentForm()},
+            )
+            
+            
+    def post(self, request, slug, *args, **kwargs):
+            queryset = Post.objects.filter(status=1)
+            post = get_object_or_404(queryset, slug=slug)
+            comments = post.comments.filter(approved=True).order_by('-created_on')
+            liked = False
+            if post.likes.filter(id=self.request.user.id).exists():
+                liked =True
+
+            comment_form = CommentForm(data=request.POST)
+
+            if comment_form.is_valid():
+                comment_form.instance.email = request.user.email
+                comment_form.instance.name = request.user.username
+                comment = comment_form.save(commit=False)
+                comment.post = post
+                comment.save()
+            else:
+                comment_form = CommentForm()
+
+            return render(
+                request, "post_detail.html", {"post": post, "comments": comments, "commented": True, "liked": liked, "comment_form": CommentForm()},
             )
 
 class PostUpload(View):
 
     def blog_upload(request):
-        blog_form = BlogForm
         if request.method == 'POST':
-            blog_form = BlogForm(request.POST)
+            blog_form = BlogForm(request.POST or None, instance=form)
             if blog_form.is_valid():
-                
-                return redirect('home')
+                blog_form.save()
+                return HttpResponseRedirect("/"+id)
         
         context = {
             'blog_form': blog_form
