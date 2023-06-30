b897e2410e9b260aa910ae94ced63e7f8210e347
meija1 <meija911@gmail.com>
2023-06-30 15:29:17 +0000

    Fix post detail slug error and post content can now be visible

--- blog/views.py
+++ blog/views.py
@@ -10,14 +10,13 @@ class PostList(generic.ListView):
     model = Post
     queryset = Post.objects.filter(status=1).order_by('-created_on')
     template_name = "index.html"
-    paginate_by = 6
 
 ''' Add view for post details '''
 class PostDetail(View):
 
-    def get(self, request, slug, *args, **kwargs):
+    def get(self, request, id, *args, **kwargs):
             queryset = Post.objects.filter(status=1)
-            post = get_object_or_404(queryset, slug=slug)
+            post = get_object_or_404(queryset, id=id)
             comments = post.comments.filter(approved=True).order_by('-created_on')
             liked = False
             if post.likes.filter(id=self.request.user.id).exists():
@@ -28,9 +27,9 @@ class PostDetail(View):
             )
             
             
-    def post(self, request, slug, *args, **kwargs):
+    def post(self, request, id, *args, **kwargs):
             queryset = Post.objects.filter(status=1)
-            post = get_object_or_404(queryset, slug=slug)
+            post = get_object_or_404(queryset, id=id)
             comments = post.comments.filter(approved=True).order_by('-created_on')
             liked = False
             if post.likes.filter(id=self.request.user.id).exists():
