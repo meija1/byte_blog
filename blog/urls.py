c8ce1cf57043b70e04757b2b6c7a2ca33e73d5b1
meija1 <meija911@gmail.com>
2023-07-10 19:15:28 +0000

    Fix url post like error and update the look of post detail site

--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,6 @@ from django.urls import path
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
     path('account/create', views.blog_upload, name='create_post'),
-    path('<id>', views.PostDetail.as_view(), name='post_detail'),
-    path('like/<slug>', views.PostLike.as_view, name='post_like'),
+    path('post_detail/<slug:slug>', views.PostDetail.as_view(), name='post_detail'),
+    path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -16,9 +16,9 @@ class PostList(generic.ListView):
 ''' Add view for post details '''
 class PostDetail(View):
 
-    def get(self, request, id, *args, **kwargs):
+    def get(self, request, slug, *args, **kwargs):
             queryset = Post.objects.filter(status=1)
-            post = get_object_or_404(queryset, id=id)
+            post = get_object_or_404(queryset, slug=slug)
             comments = post.comments.filter(approved=True).order_by('-created_on')
             liked = False
             if post.likes.filter(id=self.request.user.id).exists():
@@ -29,9 +29,9 @@ class PostDetail(View):
             )
             
             
-    def post(self, request, id, *args, **kwargs):
+    def post(self, request, slug, *args, **kwargs):
             queryset = Post.objects.filter(status=1)
-            post = get_object_or_404(queryset, id=id)
+            post = get_object_or_404(queryset, slug=slug)
             comments = post.comments.filter(approved=True).order_by('-created_on')
             liked = False
             if post.likes.filter(id=self.request.user.id).exists():
@@ -85,14 +85,23 @@ def category(request, slug):
 
 class PostLike(View):
 
-    def post(self, request, slug):
-
-        post = get_object_or_404(Post, slug=slug)
-
-        if post.likes.filter(id=request.user.id).exists():
-            post.likes.remove(request.user)
-        else:
-            post.likes.add(request.user)
-
-        return HttpResponseRedirect(reverse('post_detail', args=[slug]))
+        def get(self, request, slug, *args, **kwargs):
         
+            post = get_object_or_404(Post, slug=slug)
+            if post.likes.filter(id=request.user.id).exists():
+                post.likes.remove(request.user)
+            else:
+                post.likes.add(request.user)
+
+            return HttpResponseRedirect(reverse('post_detail', args=[slug]))
+
+
+        def post(self, request, slug, *args, **kwargs):
+
+            post = get_object_or_404(Post, slug=slug)
+            if post.likes.filter(id=request.user.id).exists():
+                post.likes.remove(request.user)
+            else:
+                post.likes.add(request.user)
+
+            return HttpResponseRedirect(reverse('post_detail', args=[slug]))
