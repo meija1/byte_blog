c04e76814cd474517f73e20466e41ac4c8b17404
meija1 <meija911@gmail.com>
2023-07-10 17:34:07 +0000

    Add path to post like and fix html code for likes

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
     path('account/create', views.blog_upload, name='create_post'),
     path('<id>', views.PostDetail.as_view(), name='post_detail'),
+    path('like/<slug>', views.PostLike.as_view, name='post_like'),
 ]
