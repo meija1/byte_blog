f1f067cfdd3cca1b9b2c7eb8813f5fc587cd5c04
meija1 <meija911@gmail.com>
2023-07-12 15:19:09 +0000

    Add url path for post update

--- blog/urls.py
+++ blog/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('account/create', views.blog_upload, name='create_post'),
     path('post_detail/<slug:slug>', views.PostDetail.as_view(), name='post_detail'),
     path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
+    path('update/<slug:slug>', views.PostUpdate.as_view(), name='post_update'),
     path('delete/<slug:slug>', views.PostDelete.as_view(), name='post_delete'),
 ]
